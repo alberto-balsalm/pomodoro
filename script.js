74bd3d0b83a08cc0ef0b1141a5754930de87739f
alberto-balsalm <wojciechpolitowski@protonmail.ch>
2020-05-18 14:15:43 +0200

    Timer switches between seesion and break modes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let countdown
-let sessionLength = 100
+let sessionLength = 15
 let timeLeft = sessionLength
+let breakLength = 5
+let countdownType = "session"
 
 const pauseButton = document.querySelector('.pause.btn')
 const stopButton = document.querySelector('.stop.btn')
 const resetButton = document.querySelector('.reset.btn')
 const startButton = document.querySelector('.start.btn')
+const info = document.querySelector('.info')
 
 /* activateStartBtn() */
 activateButtons()
@@ -19,7 +22,16 @@ function timer(seconds) {
     countdown = setInterval(() => {
         const now = Date.now()
         const secondsLeft = parseInt(Math.round((then - now) / 1000))       
-        if(secondsLeft <= 0) clearInterval(countdown)
+        if(secondsLeft <= 0) {
+            clearInterval(countdown)
+            if(countdownType === "session") {
+                countdownType = "break"
+                timer(breakLength)
+            } else {
+                countdownType = "session"
+                timer(sessionLength)
+            }
+        }
         displayTimeLeft(secondsLeft)
         timeLeft = secondsLeft
     }, 1000)
@@ -57,11 +69,15 @@ function pause() {
     clearInterval(countdown)
     pauseButton.classList.add('disabled')
     startButton.classList.remove('disabled')
+    document.title += " (paused)"
 }
 
 function stop() {
     clearInterval(countdown)
     displayTimeLeft(sessionLength)
+    timeLeft = sessionLength
+    countdownType = "session"
     document.title = "Pomodoro clock" //displayTimeLeft shows time left on a tab
     stopButton.classList.add('disabled')
+    startButton.classList.remove('disabled')
 }
